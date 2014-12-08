c24b099dca29f752e3aa213238bd60dcbf8cb75e
Lorex <joe50613@gmail.com>
2014-12-08 17:12:53 +0800

    change zh_tw to tw

--- GameEngine/Lang/tw.php
+++ GameEngine/Lang/tw.php
@@ -87,7 +87,7 @@ define("REGISTER_NW","西北");
 define("REGISTER_SE","東南");
 define("REGISTER_SW","西南");
 define("REGISTER_RANDOM","隨機");
-define("REGISTER_MOREINFO","T&C");
+define("REGISTER_MOREINFO","使用條款");
 define("REGISTER_CLOSED","本伺服器目前暫不開放註冊。");
 
 //MENU
@@ -100,120 +100,123 @@ define("TOUR","遊戲簡介");
 
 
 //ERRORS
-define("USRNM_EMPTY","(Username empty)");
-define("USRNM_TAKEN","(Name is already in use.)");
-define("USRNM_SHORT","(min. ".USRNM_MIN_LENGTH." figures)");
-define("USRNM_CHAR","(Invalid Characters)");
-define("PW_EMPTY","(Password empty)");
-define("PW_SHORT","(min. ".PW_MIN_LENGTH." figures)");
-define("PW_INSECURE","(Password insecure. Please choose a more secure one.)");
-define("EMAIL_EMPTY","(Email Empty)");
-define("EMAIL_INVALID","(Invalid email address)");
-define("EMAIL_TAKEN","(Email is already in use)");
-define("TRIBE_EMPTY","<li>Please choose a tribe.</li>");
-define("AGREE_ERROR","<li>You have to agree to the game rules and the general terms & conditions in order to register.</li>");
-define("LOGIN_USR_EMPTY","Enter name.");
-define("LOGIN_PASS_EMPTY","Enter password.");
-define("EMAIL_ERROR","Email does not match existing");
-define("PASS_MISMATCH","Passwords do not match");
-define("ALLI_OWNER","Please appoint an alliance owner before deleting");
-define("SIT_ERROR","Sitter already set");
-define("USR_NT_FOUND","Name does not exist.");
-define("LOGIN_PW_ERROR","The password is wrong.");
-define("WEL_TOPIC","Useful tips & information ");
-define("ATAG_EMPTY","Tag empty");
-define("ANAME_EMPTY","Name empty");
-define("ATAG_EXIST","Tag taken");
-define("ANAME_EXIST","Name taken");
+define("USRNM_EMPTY","(帳號不能為空)");
+define("USRNM_TAKEN","(此帳號已被使用)");
+define("USRNM_SHORT","(帳號長度最短限制：".USRNM_MIN_LENGTH." 字元)");
+define("USRNM_CHAR","(帳號含有非法字元)");
+define("PW_EMPTY","(密碼不能為空)");
+define("PW_SHORT","(帳號長度最短限制：".PW_MIN_LENGTH." 字元)");
+define("PW_INSECURE","(密碼不夠安全，請使用另外一個密碼)");
+define("EMAIL_EMPTY","(電子郵箱不能為空)");
+define("EMAIL_INVALID","(電子郵箱格式不正確)");
+define("EMAIL_TAKEN","(本組電子郵箱已被使用)");
+define("TRIBE_EMPTY","<li>請選擇種族</li>");
+define("AGREE_ERROR","<li>您必須同意使用條款方可進行註冊。</li>");
+define("LOGIN_USR_EMPTY","請輸入帳號");
+define("LOGIN_PASS_EMPTY","請輸入密碼");
+define("EMAIL_ERROR","電子郵箱不能為空");
+define("PASS_MISMATCH","密碼不正確");
+define("ALLI_OWNER","在刪除前請先指派盟主");
+define("SIT_ERROR","介紹人早已設定");
+define("USR_NT_FOUND","此帳號不存在");
+define("LOGIN_PW_ERROR","密碼錯誤");
+define("WEL_TOPIC","錦囊妙計");
+define("ATAG_EMPTY","標籤不能為空");
+define("ANAME_EMPTY","聯盟名稱不能為空");
+define("ATAG_EXIST","此標籤已被使用");
+define("ANAME_EXIST","此名稱已被使用");
 
 //COPYRIGHT
-define("TRAVIAN_COPYRIGHT","TravianX 100% Open Source Travian Clone.");
+define("TRAVIAN_COPYRIGHT","OC Travian, server by SDTServ.");
 
 //BUILD.TPL
-define("CUR_PROD","Current production");
-define("NEXT_PROD","Production at level ");
+define("CUR_PROD","目前產量");
+define("NEXT_PROD","產量在等級 ");
 
 //BUILDINGS
-define("B1","Woodcutter");
-define("B1_DESC","The Woodcutter cuts down trees in order to produce Wood. The further you extend the bid1 the more Wood is produced by him.");
-define("B1_DESC","The Woodcutter cuts down trees in order to produce lumber. The further you extend the bid1 the more lumber is produced by him.");
+define("B1","伐木場");
+define("B1_DESC","伐木場是砍伐木材的地方。伐木場等級越高，單位時間內能砍伐的木材也就越多。");
 
-define("B2","Clay Pit");
-define("B2_DESC","Clay is produced here. By increasing its level you increase its clay production.");
-define("B3","Iron Mine");
-define("B3_DESC","Here miners produce the precious resource iron. By increasing the mine`s level you increase its iron production.");
-define("B4","Cropland");
-define("B4_DESC","Your population`s food is produced here. By increasing the farm`s level you increase its crop production.");
+define("B2","泥坑");
+define("B2_DESC","泥坑是生產泥土的地方。泥坑等級越高，單位時間內生產的磚塊也就越多。");
+
+define("B3","鐵礦場");
+define("B3_DESC","鐵礦場是用來採集鋼鐵的。鐵礦場等級越高，每小時能採集的鋼鐵就越多。");
+
+define("B4","農場");
+define("B4_DESC","農場是為居民生產穀物的地方。農場等級越高，所能生產的穀物也就越多。");
 
 //DORF1
-define("WOOD","Wood");
-define("CLAY","Clay");
-define("IRON","Iron");
-define("CROP","Crop");
-define("LEVEL","Level");
-define("CROP_COM",CROP." consumption");
-define("PER_HR","per hour");
-define("PROD_HEADER","Production");
-define("MULTI_V_HEADER","Villages");
-define("ANNOUNCEMENT","Announcement");
-define("GO2MY_VILLAGE","Go to my village");
-define("VILLAGE_CENTER","Village centre");
-define("FINISH_GOLD","Finish all construction and research orders in this village immediately for 2 Gold?");
-define("WAITING_LOOP","(waiting loop)");
-define("HRS","(hrs.)");
-define("DONE_AT","done at");
-define("CANCEL","cancel");
-define("LOYALTY","Loyalty:"); 
-define("CALCULATED_IN","Calculated in");
-define("SEVER_TIME","Server time:");  
+define("WOOD","木材");
+define("CLAY","磚塊");
+define("IRON","鋼鐵");
+define("CROP","穀物");
+define("LEVEL","等級");
+define("CROP_COM","耗糧");
+define("PER_HR","每小時");
+define("PROD_HEADER","產量");
+define("MULTI_V_HEADER","村莊");
+define("ANNOUNCEMENT","公告");
+define("GO2MY_VILLAGE","返回我的村莊");
+define("VILLAGE_CENTER","城鎮中心");
+define("FINISH_GOLD","花費 2 金完成所有進行中的建築及研究?");
+define("WAITING_LOOP","(等待佇列)");
+define("HRS","(小時)");
+define("DONE_AT","完成在");
+define("CANCEL","取消");
+define("LOYALTY","忠誠度:"); 
+define("CALCULATED_IN","運算耗時");
+define("SEVER_TIME","伺服器時間:");  
 
 //QUEST
-define("Q_CONTINUE","Continue with the next task.");
-define("Q_REWARD","Your reward:");
-define("Q0","Welcome to ");
+define("Q_CONTINUE","繼續下一個任務。");
+define("Q_REWARD","任務獎勵:");
+define("Q0","歡迎來到 ");
 define("Q0_DESC","As I see you have been made chieftain of this little village. I will be your counselor for the first few days and never leave your (right hand) side.");
-define("Q0_OPT1","To the first task.");
-define("Q0_OPT2","Look around on your own.");
-define("Q0_OPT3","Play no tasks.");
+define("Q0_OPT1","開始第一個任務!");
+define("Q0_OPT2","我想先自己到處看看");
+define("Q0_OPT3","我不想進行任務");
 
-define("Q1","Task 1: Woodcutter");
-define("Q1_DESC","There are four green forests around your village. Construct a woodcutter on one of them. Wood is an important resource for our new settlement.");
-define("Q1_ORDER","Order:<\/p>Construct a woodcutter.");
-define("Q1_RESP","Yes, that way you gain more Wood.I helped a bit and completed the order instantly.");
-define("Q1_DESC","There are four green forests around your village. Construct a woodcutter on one of them. Lumber is an important resource for our new settlement.");
-define("Q1_ORDER","Order:<\/p>Construct a woodcutter.");
-define("Q1_RESP","Yes, that way you gain more lumber.I helped a bit and completed the order instantly.");
+define("Q1","任務 1: 伐木場");
+define("Q1_DESC","在您的村莊附近有４個森林。在其中一個森林裡興建伐木場。對於我們的新聚落來說，木材是一個非常重要的資源。");
+define("Q1_ORDER","任務目標:<\/p>興建伐木場。");
+define("Q1_RESP","非常好，這樣您的人民就有木材可以使用了。我會協助您立即完成你的建築進度。");
+//define("Q1_DESC","There are four green forests around your village. Construct a woodcutter on one of them. Lumber is an important resource for our new settlement.");
+//define("Q1_ORDER","任務目標:<\/p>Construct a woodcutter.");
+//define("Q1_RESP","Yes, that way you gain more lumber.I helped a bit and completed the order instantly.");
 
-define("Q1_REWARD","Woodcutter instantly completed.");
+define("Q1_REWARD","伐木場 立刻完成。");
 
-define("Q2","Task 2: Crop");
-define("Q2_DESC","Now your subjects are hungry from working all day. Extend a cropland to improve your subjects' supply. Come back here once the building is complete.");
-define("Q2_ORDER","Order:<\/p>Extend one cropland.");
-define("Q2_RESP","Very good. Now your subjects have enough to eat again...");
+define("Q2","任務 2: 穀物");
+define("Q2_DESC","您的人民因為工作了一整天，肚子已經很餓了。為了使他們更有氣力工作，您需要將一塊個農場升級，增加穀物產量。完成升級後立刻回到這裡來。");
+define("Q2_ORDER","任務目標:<\/p>升級一個農場。");
+define("Q2_RESP","非常好。現在您的人民又有足夠的食糧了...");
 
-define("Q3","Task 3: Your Village's Name");
-define("Q3_DESC","Creative as you are you can grant your village the ultimate name.\r\n<br \/><br \/>\r\nClick on 'profile' in the left hand menu and then select 'change profile'...");
-define("Q3_ORDER","Order:<\/p>Change your village's name to something nice.");
-define("Q3_RESP","Wow, a great name for their village. It could have been the name of my village!...");
+define("Q3","任務 3: 您的村莊名稱");
+define("Q3_DESC","請運用您的創作力，為您的村莊起一個名稱。\r\n<br \/><br \/>\r\n在左方選單按「玩家資料」，然後選「個人資料」...");
+define("Q3_ORDER","任務目標:<\/p>或許您可以幫您的村莊起一個更好的名稱。");
+define("Q3_RESP","英雄所見略同！這是一個美好的村莊名稱。");
 
-define("Q4","Task 4: Other Players");
-define("Q4_DESC","In ". SERVER_NAME ." you play along with billions of other players. Click 'statistics' in the top menu to look up your rank and enter it here.");
-define("Q4_ORDER","Order:<\/p>Look for your rank in the statistics and enter it here.");
-define("Q4_BUTN","complete task");
-define("Q4_RESP","Exactly! That's your rank.");
+define("Q4","任務 4: 其他玩家");
+define("Q4_DESC","您在 ". SERVER_NAME ." 會和無數玩家互動。在頂部清單按「統計」查閱自己的排名，然後在此輸入。");
+define("Q4_ORDER","任務目標:<\/p>請查閱您的排名，然後在此輸入它");
+define("Q4_BUTN","完成任務");
+define("Q4_RESP","答對了！這就是您的排名。");
 
-define("Q5","Task 5: Two Building Orders");
-define("Q5_DESC","Build an iron mine and a clay pit. Of iron and clay one can never have enough.");
-define("Q5_ORDER","Order:<\/p><ul><li>Extend one iron mine.<\/li><li>Extend one clay pit.<\/li><\/ul>");
-define("Q5_RESP","As you noticed, building orders take rather long. The world of ". SERVER_NAME ." will continue to spin even if you are offline. Even in a few months there will be many new things for you to discover.\r\n<br \/><br \/>\r\nThe best thing to do is occasionally checking your village and giving you subjects new tasks to do.");
+define("Q5","任務 5: 兩項建築任務");
+define("Q5_DESC","您需要興建一個鐵礦場和一個泥坑。要知道，鋼鐵和磚塊常常不足。");
+define("Q5_ORDER","任務目標:<\/p><ul><li>* 升級一個鐵礦場<\/li><li>* 升級一個泥坑<\/li><\/ul>");
+define("Q5_RESP","您應該發現，完成建築任務需要頗長時間。但即使您在離線狀態，". SERVER_NAME ." 世界依然繼續運作；就算只是數個月的時間，這裡總會有很多新奇事物讓您發現。\r\n<br \/><br \/>\r\n最好偶爾留意一下您的村莊以及委派新任務給您的人民。");
 
-define("Q6","Message From The Taskmaster");
-define("Q6_DESC","You are to be informed that a nice reward is waiting for you at the taskmaster.<br /><br />Hint: The message has been generated automatically. An answer is not necessary.");
+define("Q6","訊息");
+define("Q6_DESC","您可以透過通訊系統與其他玩家談話。我已送出一條訊息給您，看完後再回到這裡。");
+define("Q6_ORDER","任務目標:<\/p>看新訊息");
+define("Q6_RESP","您收到了？很好");
 
-define("Q5","Task 5: Two Building Orders");
-define("Q5_DESC","Build an iron mine and a clay pit. Of iron and clay one can never have enough.");
-define("Q5_ORDER","Order:<\/p><ul><li>Extend one iron mine.<\/li><li>Extend one clay pit.<\/li><\/ul>");
-define("Q5_RESP","As you noticed, building orders take rather long. The world of ". SERVER_NAME ." will continue to spin even if you are offline. Even in a few months there will be many new things for you to discover.\r\n<br \/><br \/>\r\nThe best thing to do is occasionally checking your village and giving you subjects new tasks to do.");
+//define("Q5","任務 5: Two Building Orders");
+//define("Q5_DESC","Build an iron mine and a clay pit. Of iron and clay one can never have enough.");
+//define("Q5_ORDER","任務目標:<\/p><ul><li>Extend one iron mine.<\/li><li>Extend one clay pit.<\/li><\/ul>");
+//define("Q5_RESP","As you noticed, building orders take rather long. The world of ". SERVER_NAME ." will continue to spin even if you are offline. Even in a few months there will be many new things for you to discover.\r\n<br \/><br \/>\r\nThe best thing to do is occasionally checking your village and giving you subjects new tasks to do.");
 
 //======================================================//
 //================ UNITS - DO NOT EDIT! ================//
